5f971bc26c5cfcc7ebeaa6ccf6d3c87a311051db
Ignasuss <146200458+Ignasuss@users.noreply.github.com>
2024-01-25 00:20:16 +0200

    pabaigu pabaiga

--- AZ Tanks/Assets/Scipts/Player 1.cs
+++ AZ Tanks/Assets/Scipts/Player 1.cs
@@ -1,37 +1,56 @@
 using UnityEngine;
 
-public class Player1 : MonoBehaviour
+public class Player2 : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed;
     private Rigidbody2D rb;
+    public float rotateSpeed;
     private Vector2 movement;
-
+    public string verticalAxis;
+    public string horizontalAxis;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         // Prevent the character from spinning upon collision
-        rb.freezeRotation = true;
     }
 
     void Update()
     {
         // Input
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        //movement.x = Input.GetKey(KeyCode.L) ? 1 : Input.GetKey(KeyCode.J) ? -1 : 0;
+        //movement.y = Input.GetKey(KeyCode.I) ? 1 : Input.GetKey(KeyCode.K) ? -1 : 0;
 
-        // Rotate the character to face the direction of movement
-        if (movement != Vector2.zero)
+        float angleInRadians = Mathf.Deg2Rad * (transform.rotation.eulerAngles.z-90f);
+        if (Input.GetKey(KeyCode.W))
         {
-            float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, 0f, angle - 90); // Subtract 90 to adjust for sprite's upward orientation
+            
+            transform.position = new Vector2(transform.position.x-Mathf.Cos(angleInRadians)*Time.deltaTime*moveSpeed, transform.position.y - Mathf.Sin(angleInRadians) * Time.deltaTime*moveSpeed);
         }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = new Vector2(transform.position.x + Mathf.Cos(angleInRadians) * Time.deltaTime*moveSpeed, transform.position.y + Mathf.Sin(angleInRadians) * Time.deltaTime*moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, 0, -rotateSpeed * Time.deltaTime);
+        }
+        // Rotate the character to face the direction of movement
+        //if (movement != Vector2.zero)
+        //{
+        //    float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
+        //    transform.rotation = Quaternion.Euler(0f, 0f, angle - 90); // Subtract 90 to adjust for sprite's upward orientation
+        //}
     }
 
-    void FixedUpdate()
-    {
-        
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-    }
+    //void FixedUpdate()
+    //{
+      //  rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    //}
 
 }
 
